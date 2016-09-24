e626aa09799361b6e6e437c24c00bb8612f1b364
Boullon Daniel <daniel.boullon@yahoo.com>
2016-09-24 14:27:38 -0300

    Create ConEspadaSkofnung.java

---
+++ Juego/src/juego/ConEspadaSkofnung.java
@@ -0,0 +1,29 @@
+package juego;
+
+public class ConEspadaSkofnung extends PersonajeEquipado {
+	
+	protected int ataque = 5;
+
+	public ConEspadaSkofnung(Personaje personaje) {
+		super(personaje);
+	}
+
+	@Override
+	public int obtenerPuntosDeAtaque() {
+		return ataque + super.obtenerPuntosDeAtaque();
+	}
+
+	@Override
+	protected int calcularPuntosDeAtaque() {
+		return super.calcularPuntosDeAtaque();
+	}
+
+	@Override
+	public boolean puedeAtacar() {
+		return super.puedeAtacar();
+	}
+
+
+	
+
+}
